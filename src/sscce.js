fb36f9aac1d3af479854562c6a96cc1c79e45448
Pedro Augusto de Paula Barbosa <papb1996@gmail.com>
2019-10-07 21:41:45 -0300

    SSCCE for issue 11467

--- src/sscce.js
+++ src/sscce.js
@@ -1,68 +1,46 @@
 'use strict';
 
-/**
- * Your SSCCE goes inside this function.
- * 
- * Please do everything inside it, including requiring dependencies.
- * 
- * Two parameters, described below, are automatically passed to this
- * function for your convenience. You should use them in your SSCCE.
- * 
- * @param {function(options)} createSequelizeInstance This parameter
- * is a function that you should call to create the sequelize instance
- * for you. You should use this instead of `new Sequelize(...)` since
- * it will automatically setup every dialect in order to automatically
- * run it on all dialects once you push it to GitHub (by using Travis
- * CI and AppVeyor). You can pass options to this function, they will
- * be sent to the Sequelize constructor.
- * 
- * @param {function} log This is a convenience function to log results
- * from queries in a clean way, without all the clutter that you would
- * get from simply using `console.log`. You should use it whenever you
- * would use `console.log` unless you have a good reason not to do it.
- */
 module.exports = async function(createSequelizeInstance, log) {
-    /**
-     * Below is an example of SSCCE. Change it to your SSCCE.
-     * Recall that SSCCEs should be minimal! Try to make the shortest
-     * possible code to show your issue. The shorter your code, the
-     * more likely it is for you to get a fast response.
-     */
-
-    // Require necessary things from Sequelize
+    // SSCCE for #11467
     const { Sequelize, Op, Model, DataTypes } = require('sequelize');
-
-    // Create an instance, using the convenience function instead
-    // of the usual instantiation with `new Sequelize(...)`
     const sequelize = createSequelizeInstance({ benchmark: true });
-
-    // You can use await in your SSCCE!
     await sequelize.authenticate();
 
-    // Define some models and whatever you need for your SSCCE.
-    // Note: recall that SSCCEs should be minimal! Try to make the
-    // shortest possible code to show your issue. The shorter your
-    // code, the more likely it is for you to get a fast response
-    // on your issue.
-    const User = sequelize.define('User', {
-        name: DataTypes.TEXT,
-        pass: DataTypes.TEXT
+    const Project = sequelize.define('tst_project', {
+        title: Sequelize.STRING,
+        description: Sequelize.TEXT,
+        text: {
+            type: new Sequelize.VIRTUAL(Sequelize.STRING, ['title', 'description']),
+            get: function() {
+                return this.get('title') + ': ' + this.get('description');
+            }
+        }
     });
-    const Foo = sequelize.define('Foo', {
-        name: DataTypes.TEXT,
-        pass: DataTypes.TEXT
+    const Task = sequelize.define('tst_task', {
+        title: Sequelize.STRING,
+        description: Sequelize.TEXT,
+        deadline: Sequelize.DATE,
+        text: {
+            type: new Sequelize.VIRTUAL(Sequelize.STRING, ['title', 'id']),
+            get: function() {
+                return this.get('title') + ': ' + this.get('id');
+            }
+        }
     });
-    User.belongsTo(Foo);
-    Foo.hasOne(User);
+    Task.belongsTo(Project, { foreignKey: 'tstProjectId', targetKey: 'id', as: 'Project' });
 
-    // Since you defined some models above, don't forget to sync them.
-    // Using the `{ force: true }` option is not necessary because the
-    // database is always created from scratch when the SSCCE is
-    // executed after pushing to GitHub (by Travis CI and AppVeyor).
     await sequelize.sync();
 
-    // Call your stuff to show the problem...
-    log(await User.findAll()); // The result is empty!! :O
-    // Of course in this case it is not a bug, we didn't insert
-    // anything!
+    let project = await Project.create({ title: 'Project 1', description: 'Project 1 description' });
+    let task = await Task.create({ tstProjectId: 1, title: 'Task 1', description: 'Task 1 description' });
+    let results = await Task.findAll({
+        attributes: ['id'],
+        include: [{
+            model: Project,
+            attributes: ['id', 'text'],
+            as: 'Project'
+        }]
+    });
+    log("Test");
+    log(results[0].get('Project').get('text'));
 };
