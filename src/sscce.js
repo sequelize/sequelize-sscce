b7767541cb08d8db72d0240496a0dca8d79ff308
Andrew Burgess <andrew@burgess.io>
2019-10-10 14:23:29 -0400

    update example

--- src/sscce.js
+++ src/sscce.js
@@ -13,22 +13,21 @@ module.exports = async function(createSequelizeInstance, log) {
 
     await sequelize.sync();
   
-    const u1 = await User.create({
+    await User.create({
       name: '$user1'
     });
-    const u2 = await User.create({
+    await User.create({
       name: sequelize.fn('upper', 'user2')
     });
-    let u3;
+
     try {
-      u3 = await User.create({
+      await User.create({
         name: sequelize.fn('upper', '$user3')
       });
     } catch (error) {
       log(error.message);
-      u3 = {};
     }
-    log(u1.name);
-    log(u2.name);
-    log(u3.name);
+
+    const users = User.findAll({}, { plain: true });
+    log(users);
 };
