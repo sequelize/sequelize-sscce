5176214c583edb090dafa0e4b6ea37cf1159656c
Pedro Augusto de Paula Barbosa <papb1996@gmail.com>
2019-10-20 19:32:54 -0300

    Test JSON

--- src/sscce.js
+++ src/sscce.js
@@ -1,33 +1,22 @@
 'use strict';
-
-function check(type, str, sequelize) {
-  let name = ('foo' + Math.random()).replace(/\./g, '');
-  try {
-    console.log('trying ' + str);
-    sequelize.define(name, { test: type });
-    console.log(str + ' worked');
-  } catch (e) {
-    console.log(str + " didn't work:", e.message);
-  }
-}
-
 module.exports = async function(createSequelizeInstance, log) {
     const { Sequelize, Op, Model, DataTypes } = require('sequelize');
     const sequelize = createSequelizeInstance();
-    await sequelize.authenticate();
-    check(DataTypes.INTEGER.UNSIGNED, 'DataTypes.INTEGER.UNSIGNED', sequelize);
-    check(DataTypes.INTEGER.ZEROFILL, 'DataTypes.INTEGER.ZEROFILL', sequelize);
-    check(DataTypes.INTEGER.UNSIGNED.ZEROFILL, 'DataTypes.INTEGER.UNSIGNED.ZEROFILL', sequelize);
-    check(DataTypes.INTEGER.ZEROFILL.UNSIGNED, 'DataTypes.INTEGER.ZEROFILL.UNSIGNED', sequelize);
-    check(DataTypes.INTEGER(10).UNSIGNED, 'DataTypes.INTEGER(10).UNSIGNED', sequelize);
-    check(DataTypes.INTEGER(10).ZEROFILL, 'DataTypes.INTEGER(10).ZEROFILL', sequelize);
-    check(DataTypes.INTEGER(10).UNSIGNED.ZEROFILL, 'DataTypes.INTEGER(10).UNSIGNED.ZEROFILL', sequelize);
-    check(DataTypes.INTEGER(10).ZEROFILL.UNSIGNED, 'DataTypes.INTEGER(10).ZEROFILL.UNSIGNED', sequelize);
-    check(DataTypes.INTEGER().UNSIGNED, 'DataTypes.INTEGER().UNSIGNED', sequelize);
-    check(DataTypes.INTEGER().ZEROFILL, 'DataTypes.INTEGER().ZEROFILL', sequelize);
-    check(DataTypes.INTEGER().UNSIGNED.ZEROFILL, 'DataTypes.INTEGER().UNSIGNED.ZEROFILL', sequelize);
-    check(DataTypes.INTEGER().ZEROFILL.UNSIGNED, 'DataTypes.INTEGER().ZEROFILL.UNSIGNED', sequelize);
-    await sequelize.authenticate();
+    const Foo = sequelize.define('foo', { data: DataTypes.JSON }, { timestamps: false });
     await sequelize.sync();
-    await sequelize.authenticate();
+
+    await Foo.create({
+        data: {
+            a: 1,
+            b: [2, 3],
+            c: {
+                d: 4
+            }
+        }
+    });
+    const foo = await Foo.findOne();
+    log("foo", foo);
+    log("foo.data", foo.data);
+    log("typeof foo.data", typeof foo.data);
+    log("Object.keys(foo.data)", Object.keys(foo.data));
 };
