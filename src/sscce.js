b27c57c3eb7f268a2274d8266f6cd5750fab73e6
Pedro Augusto de Paula Barbosa <papb1996@gmail.com>
2019-12-23 15:47:02 -0300

    15:46

--- src/sscce.js
+++ src/sscce.js
@@ -7,9 +7,10 @@ module.exports = async function(createSequelizeInstance, log) {
     const { Sequelize, Op, Model, DataTypes } = require('sequelize');
     const sequelize = createSequelizeInstance({ benchmark: true, logQueryParameters: true });
     const Foo = sequelize.define('Foo', { name: DataTypes.STRING });
-    await sequelize.sync();
     log(await sequelize.query(`SET TIMEZONE TO 'CET'`));
+    log(await sequelize.sync());
     log(await sequelize.query(`SELECT NOW()`));
     log(await Foo.create({ name: 'xyz' }));
     log(await sequelize.query(`SELECT "createdAt" FROM "Foos"`));
+    log(await Foo.findOne({ plain: true, raw: true }));
 };
