cae656678edcf94cc508887a9aa8c3a85d8a9054
papb <papb1996@gmail.com>
2019-12-26 19:49:51 -0300

    test

--- src/sscce.js
+++ src/sscce.js
@@ -3,11 +3,23 @@
 module.exports = async function(createSequelizeInstance, log) {
     const { Sequelize, Op, Model, DataTypes } = require('sequelize');
     const sequelize = createSequelizeInstance({ logQueryParameters: true, benchmark: true });
-    const Foo = sequelize.define('Foo', { name: DataTypes.STRING });
-    log(await sequelize.query(`SET TIMEZONE TO 'CET'`));
-    log(await sequelize.sync());
-    log(await sequelize.query(`SELECT NOW()`));
-    log(await Foo.create({ name: 'xyz' }));
-    log(await sequelize.query(`SELECT "createdAt" FROM "Foos"`));
-    log(await Foo.findOne({ plain: true, raw: true }));
+    const queryInterface = sequelize.getQueryInterface();
+
+    await queryInterface.createTable('Person', {
+        name: DataTypes.STRING,
+        isBetaMember: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false
+        }
+    });
+
+    await queryInterface.addColumn('Person', 'petName', { type: DataTypes.STRING });
+    await queryInterface.changeColumn('Person', 'petName', { type: DataTypes.TEXT('medium') });
+    await queryInterface.changeColumn('Person', 'petName', { type: DataTypes.INTEGER });
+    await queryInterface.addColumn('Person', 'petName', {
+        type: DataTypes.TINYINT(3),
+        defaultValue: 2,
+        allowNull: false
+    });
 };
