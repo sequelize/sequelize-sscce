4363b8acd21e32bffecb0b7e38adca9eb6c00a0c
Pedro Augusto de Paula Barbosa <papb1996@gmail.com>
2020-01-04 18:11:22 -0300

    Refactor (#32)

--- setup/runner.js
+++ setup/runner.js
@@ -2,38 +2,28 @@
 
 require('./global-adjusts');
 
-const jsonStringifySafe = require('json-stringify-safe');
-
-function log(...args) {
-    console.log.apply(console, args.map(arg => {
-        return jsonStringifySafe(arg, null, 2);
-    }));
-}
-
 async function run() {
-    const createSequelizeInstance = require('./create-sequelize-instance');
-
-    console.log('\n\n' + '-'.repeat(30) + '\n\n');
+    console.log('\n' + '-'.repeat(30) + '\n');
 
     if (process.env.LOCAL_SSCCE) {
-        console.gold('Warning: running the SSCCE locally will use SQLite only. To run your SSCCE in all dialects, just configure Travis and Appveyor in your GitHub repository.\n\n');
+        console.gold('Warning: running the SSCCE locally will use SQLite only. To run your SSCCE in all dialects, just configure Travis CI / AppVeyor in your GitHub repository.\n');
     }
 
     console.blue(`===== Running SSCCE for ${process.env.DIALECT.toUpperCase()} =====`);
 
-    console.log('\n\n' + '-'.repeat(30) + '\n\n');
+    console.log('\n' + '-'.repeat(30) + '\n');
 
-    await require('./../src/sscce')(createSequelizeInstance, log);
+    await require('./../src/sscce')();
 }
 
 (async () => {
     try {
         await run();
-        console.log('\n\n' + '-'.repeat(30) + '\n\n');
+        console.log('\n' + '-'.repeat(30) + '\n');
         console.green('SSCCE done without errors!');
     } catch (e) {
         console.red(e);
-        console.log('\n\n' + '-'.repeat(30) + '\n\n');
+        console.log('\n' + '-'.repeat(30) + '\n');
         console.red('SSCCE done with error (see above).');
         process.exit(1); // eslint-disable-line no-process-exit
     }
--- src/sscce.js
+++ src/sscce.js
@@ -1,68 +1,22 @@
 'use strict';
 
-/**
- * Your SSCCE goes inside this function.
- * 
- * Please do everything inside it, including requiring dependencies.
- * 
- * Two parameters, described below, are automatically passed to this
- * function for your convenience. You should use them in your SSCCE.
- * 
- * @param {function(options)} createSequelizeInstance This parameter
- * is a function that you should call to create the sequelize instance
- * for you. You should use this instead of `new Sequelize(...)` since
- * it will automatically setup every dialect in order to automatically
- * run it on all dialects once you push it to GitHub (by using Travis
- * CI and AppVeyor). You can pass options to this function, they will
- * be sent to the Sequelize constructor.
- * 
- * @param {function} log This is a convenience function to log results
- * from queries in a clean way, without all the clutter that you would
- * get from simply using `console.log`. You should use it whenever you
- * would use `console.log` unless you have a good reason not to do it.
- */
-module.exports = async function(createSequelizeInstance, log) {
-    /**
-     * Below is an example of SSCCE. Change it to your SSCCE.
-     * Recall that SSCCEs should be minimal! Try to make the shortest
-     * possible code to show your issue. The shorter your code, the
-     * more likely it is for you to get a fast response.
-     */
+// Require the necessary things from Sequelize
+const { Sequelize, Op, Model, DataTypes } = require('sequelize');
 
-    // Require the necessary things from Sequelize
-    const { Sequelize, Op, Model, DataTypes } = require('sequelize');
+// This function should be used instead of `new Sequelize()`.
+// It applies the config for your SSCCE to work on CI.
+const createSequelizeInstance = require('./utils/create-sequelize-instance');
 
-    // Create an instance, using the convenience function instead
-    // of the usual instantiation with `new Sequelize(...)`
-    const sequelize = createSequelizeInstance({ logQueryParameters: true, benchmark: true });
+// This is a utility logger that should be preferred over `console.log()`.
+const log = require('./utils/log');
 
-    // You can use await in your SSCCE!
-    await sequelize.authenticate();
-
-    // Define some models and whatever you need for your SSCCE.
-    // Note: recall that SSCCEs should be minimal! Try to make the
-    // shortest possible code to show your issue. The shorter your
-    // code, the more likely it is for you to get a fast response
-    // on your issue.
-    const User = sequelize.define('User', {
-        name: DataTypes.TEXT,
-        pass: DataTypes.TEXT
+// Your SSCCE goes inside this function.
+module.exports = async function() {
+    const sequelize = createSequelizeInstance({
+        logQueryParameters: true,
+        benchmark: true
     });
-    const Foo = sequelize.define('Foo', {
-        name: DataTypes.TEXT,
-        pass: DataTypes.TEXT
-    });
-    User.belongsTo(Foo);
-    Foo.hasOne(User);
-
-    // Since you defined some models above, don't forget to sync them.
-    // Using the `{ force: true }` option is not necessary because the
-    // database is always created from scratch when the SSCCE is
-    // executed after pushing to GitHub (by Travis CI and AppVeyor).
+    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
     await sequelize.sync();
-
-    // Call your stuff to show the problem...
-    log(await User.findAll()); // The result is empty!! :O
-    // Of course in this case it is not a bug, we didn't insert
-    // anything!
+    log(await Foo.create({ name: 'foo' }));
 };
