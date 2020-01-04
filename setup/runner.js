af7da1326fa7779627a5f69830d4d0a4db4f0392
papb <papb1996@gmail.com>
2020-01-04 17:33:32 -0300

    refactor

--- setup/runner.js
+++ setup/runner.js
@@ -3,15 +3,15 @@
 require('./global-adjusts');
 
 async function run() {
-    console.log('\n\n' + '-'.repeat(30) + '\n\n');
+    console.log('\n' + '-'.repeat(30) + '\n');
 
     if (process.env.LOCAL_SSCCE) {
-        console.gold('Warning: running the SSCCE locally will use SQLite only. To run your SSCCE in all dialects, just configure Travis and Appveyor in your GitHub repository.\n\n');
+        console.gold('Warning: running the SSCCE locally will use SQLite only. To run your SSCCE in all dialects, just configure Travis CI / AppVeyor in your GitHub repository.\n');
     }
 
     console.blue(`===== Running SSCCE for ${process.env.DIALECT.toUpperCase()} =====`);
 
-    console.log('\n\n' + '-'.repeat(30) + '\n\n');
+    console.log('\n' + '-'.repeat(30) + '\n');
 
     await require('./../src/sscce')();
 }
@@ -19,11 +19,11 @@ async function run() {
 (async () => {
     try {
         await run();
-        console.log('\n\n' + '-'.repeat(30) + '\n\n');
+        console.log('\n' + '-'.repeat(30) + '\n');
         console.green('SSCCE done without errors!');
     } catch (e) {
         console.red(e);
-        console.log('\n\n' + '-'.repeat(30) + '\n\n');
+        console.log('\n' + '-'.repeat(30) + '\n');
         console.red('SSCCE done with error (see above).');
         process.exit(1); // eslint-disable-line no-process-exit
     }
