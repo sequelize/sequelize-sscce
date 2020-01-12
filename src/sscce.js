a0353cfd8552731797ba851c4e2dd2d82df2fbff
Synthetic Dream <chengluyu@live.cn>
2020-01-12 16:42:22 +0800

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -13,13 +13,67 @@ const log = require('./utils/log');
 // Your SSCCE goes inside this function.
 module.exports = async function() {
     const sequelize = createSequelizeInstance({
-        logQueryParameters: true,
-        benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
-        }
+      logQueryParameters: true,
+      benchmark: true,
+      define: {
+          timestamps: false // For less clutter in the SSCCE
+      },
+      dialect: 'sqlite',
+      storage: ':memory:'
     });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+  
+    const User = sequelize.define('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      username: {
+        type: Sequelize.STRING
+      }
+    });
+
+    const Task = sequelize.define('tasks', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: 'Untitled Task',
+      },
+      description: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      }
+    });
+
+    const Participation = sequelize.define('participates', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }
+    })
+
+    User.belongsToMany(Task, {
+      through: Participation,
+      as: { singular: 'user', plural: 'users' },
+    });
+
+    Task.belongsToMany(User, {
+      through: Participation,
+      as: { singular: 'task', plural: 'tasks' },
+    });
+
     await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
+    const result = await User.findAll({
+      include: [Task.associations.tasks],
+      through: {
+        attributes: ['role']
+      }
+    })
+    log(result);
 };
