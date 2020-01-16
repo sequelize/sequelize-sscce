39c956c90853469a286431da479882164d1e7409
papb <papb1996@gmail.com>
2020-01-16 14:04:54 -0300

    SSCCE for 6711
    Credits to eikaramba:
    
    https://github.com/sequelize/sequelize/issues/6711#issue-183282542

--- src/sscce.js
+++ src/sscce.js
@@ -19,7 +19,67 @@ module.exports = async function() {
             timestamps: false // For less clutter in the SSCCE
         }
     });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+
+    const Animal = sequelize.define('Animal', {
+        name: DataTypes.STRING,
+        age: DataTypes.INTEGER
+    });
+    const Kingdom = sequelize.define('Kingdom', {
+        name: DataTypes.STRING
+    });
+    const AnimalKingdom = sequelize.define('AnimalKingdom', {
+        id: {
+            type: DataTypes.INTEGER,
+            // allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        relation: DataTypes.STRING,
+        mutation: DataTypes.BOOLEAN
+    });
+    Kingdom.belongsToMany(Animal, { through: AnimalKingdom });
+    Animal.belongsToMany(Kingdom, { through: AnimalKingdom });
+
     await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
+    const [a1, a2, a3, a4] = await Animal.bulkCreate([
+        { name: 'Dog', age: 20 },
+        { name: 'Cat', age: 30 },
+        { name: 'Peacock', age: 25 },
+        { name: 'Fish', age: 100 }
+    ]);
+    const [k1, k2, k3, k4] = await Kingdom.bulkCreate([
+        { name: 'Earth' },
+        { name: 'Water' },
+        { name: 'Wind' },
+        { name: 'Space' }
+    ]);
+    await k1.addAnimals([a1, a2]);
+    await k2.addAnimals([a4]);
+    await k3.addAnimals([a3]);
+
+    const kingdoms = await Kingdom.findAll({
+        group: ['Kingdom.id'],
+        attributes: {
+            include: [
+                'Kingdom.id',
+                [
+                    Sequelize.fn('COUNT', Sequelize.col('Animals.id')),
+                    'AnimalCount'
+                ]
+            ]
+        },
+        include: {
+            model: Animal,
+            attributes: [],
+            through: { attributes: [] },
+            duplicating: false
+        }
+    });
+
+    for (const kingdom of kingdoms) {
+        console.log(`Now checking kingdom '${kingdom.name}':`);
+        console.log(`kingdom.dataValues.AnimalCount should exist: ${kingdom.dataValues.AnimalCount ? 'ok' : 'not ok'}`);
+        console.log(`kingdom.dataValues.Animals should not exist: ${kingdom.dataValues.Animals ? 'not ok' : 'ok'}`);
+    }
+
 };
