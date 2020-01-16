a6e2fe26e546bf5808959963d8b42e4d30d50eeb
Igor Yakutovich <igor@yakutovi.ch>
2020-01-16 13:13:14 +0300

    compliance with new sscce  api

--- src/sscce.js
+++ src/sscce.js
@@ -1,27 +1,8 @@
 'use strict';
 
-/**
- * Your SSCCE goes inside this function.
- *
- * Please do everything inside it, including requiring dependencies.
- *
- * Two parameters, described below, are automatically passed to this
- * function for your convenience. You should use them in your SSCCE.
- *
- * @param {function(options)} createSequelizeInstance This parameter
- * is a function that you should call to create the sequelize instance
- * for you. You should use this instead of `new Sequelize(...)` since
- * it will automatically setup every dialect in order to automatically
- * run it on all dialects once you push it to GitHub (by using Travis
- * CI and AppVeyor). You can pass options to this function, they will
- * be sent to the Sequelize constructor.
- *
- * @param {function} log This is a convenience function to log results
- * from queries in a clean way, without all the clutter that you would
- * get from simply using `console.log`. You should use it whenever you
- * would use `console.log` unless you have a good reason not to do it.
- */
-module.exports = async function(createSequelizeInstance, log) {
+const createSequelizeInstance = require('./utils/create-sequelize-instance');
+
+module.exports = async function() {
     if (process.env.DIALECT !== "postgres") return;
 
     const sequelize = createSequelizeInstance({ benchmark: true });
