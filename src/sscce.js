6d4a4d37eaf89290731e4126225414e27a4b67cf
Pedro Augusto de Paula Barbosa <papb1996@gmail.com>
2020-03-13 17:32:04 -0300

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -17,13 +17,50 @@ const { expect } = require('chai');
 module.exports = async function() {
     const sequelize = createSequelizeInstance({
         logQueryParameters: true,
-        benchmark: true,
         define: {
             timestamps: false // For less clutter in the SSCCE
         }
     });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+
+    const User = sequelize.define('user', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: DataTypes.STRING
+    }, { underscored: true });
+
+    const Project = sequelize.define('projects', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+    }, { underscored: true });
+
+    User.belongsTo(Project);
+  
     await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+    await Project.bulkCreate([{ name: 'test 1' }, { name: 'test 2' }]);
+
+    // The outcome of this SSCCE in Sequelize v4 is very different from v5!
+
+    // This will not work and project_id will not be set (NULL)
+    await User.create({ first_name: 'firstUser', last_name: 'firstUser', project_id: 1 });
+    // This will work and it will set project_id to 2
+    await User.create({ first_name: 'secondUser', last_name: 'secondUser', projectId: 2 });
+
+    log(await Project.findAll());
+    log(await User.findAll({ include: Project }));
 };
