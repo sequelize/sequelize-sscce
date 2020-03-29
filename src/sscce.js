2063ea95d724fd108fe585a0fe7f8ddbbfa34031
aristov <vv.aristov@gmail.com>
2020-03-29 19:26:22 +0300

    Create example

--- src/sscce.js
+++ src/sscce.js
@@ -1,29 +1,42 @@
-'use strict';
+'use strict'
 
 // Require the necessary things from Sequelize
-const { Sequelize, Op, Model, DataTypes } = require('sequelize');
+const { Sequelize, Op, Model, DataTypes } = require('sequelize')
 
 // This function should be used instead of `new Sequelize()`.
 // It applies the config for your SSCCE to work on CI.
-const createSequelizeInstance = require('./utils/create-sequelize-instance');
+const createSequelizeInstance = require('./utils/create-sequelize-instance')
 
 // This is an utility logger that should be preferred over `console.log()`.
-const log = require('./utils/log');
+const log = require('./utils/log')
 
 // You can use chai assertions directly in your SSCCE if you want.
-const { expect } = require('chai');
+const { assert } = require('chai')
 
 // Your SSCCE goes inside this function.
 module.exports = async function() {
-    const sequelize = createSequelizeInstance({
-        logQueryParameters: true,
-        benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
-        }
-    });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
-    await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
-};
+  const sequelize = createSequelizeInstance({
+    logQueryParameters : true,
+    benchmark : true,
+    define : {
+      timestamps : false, // For less clutter in the SSCCE
+    },
+  })
+  const Service = sequelize.define('Service', { name : DataTypes.STRING })
+  const Tariff = sequelize.define('Tariff', { name : DataTypes.STRING })
+  await sequelize.sync()
+  Service.findAll({
+    attributes : [
+      'id',
+      'name',
+      [sequelize.fn('COUNT', sequelize.col('tariffs.id')), 'tariffsTotal'],
+    ],
+    include : Tariff,
+    group : ['Service.id'],
+    order : [['id', 'DESC']],
+    limit : 5,
+    offset : 2,
+  })
+  .then(log)
+  .catch(err => assert.fail(err.message))
+}
