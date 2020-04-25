91e9d2a911ce7fd50be51990d2532fe0df9d52ee
stoyaneft <stoyaneft@gmail.com>
2020-04-25 21:56:36 +0300

    SSCCE for issue 12172

--- src/sscce.js
+++ src/sscce.js
@@ -14,7 +14,7 @@ const log = require('./utils/log');
 const { expect } = require('chai');
 
 // Your SSCCE goes inside this function.
-module.exports = async function() {
+module.exports = async function () {
     const sequelize = createSequelizeInstance({
         logQueryParameters: true,
         benchmark: true,
@@ -22,8 +22,81 @@ module.exports = async function() {
             timestamps: false // For less clutter in the SSCCE
         }
     });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
-    await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+
+    const Player = sequelize.define(
+        'players',
+        {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+            },
+            name: Sequelize.STRING,
+            teamId: Sequelize.INTEGER,
+        }
+    );
+
+    const Child = sequelize.define(
+        'children',
+        {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+            },
+            name: Sequelize.STRING,
+            playerId: Sequelize.INTEGER,
+        }
+    );
+
+    const Team = sequelize.define(
+        'teams',
+        {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+            },
+            name: Sequelize.STRING,
+            country: Sequelize.STRING,
+        }
+    );
+
+    Player.belongsTo(Team, { as: 'team', foreignKey: 'teamId' });
+    Player.hasMany(Child, { as: 'children', foreignKey: 'playerId' });
+
+    Player.addScope('children', {
+        include: [
+            {
+                model: Child.unscoped(),
+                as: 'children',
+            },
+        ],
+    });
+    Player.addScope('team', {
+        include: [
+            {
+                model: Team.unscoped(),
+                as: 'team',
+            },
+        ],
+    });
+    Player.addScope('teamByName', name => ({
+        include: [{ model: Team.unscoped(), as: 'team', where: { name } }],
+    }));
+
+    await sequelize.sync({ force: true });
+
+    const roma = await Team.create({ id: 1, name: 'Roma' });
+    const liverpool = await Team.create({ id: 2, name: 'Liverpool' });
+    await Player.create({ id: 1, name: 'Franchesko Totti', teamId: roma.id });
+    await Player.create({ id: 2, name: 'Mohamed Salah', teamId: liverpool.id });
+
+    // Appears that this query somehow pollutes the scope for the next query.
+    // If we change the order of 'children' and 'team' scopes or remove the 'children'
+    // scope the problem disappears.
+    await Player.scope(['children', 'team', { method: ['teamByName', 'Roma'] }]).findAll({
+        where: {},
+    });
+
+    const players = await Player.scope(['team']).findAll({ where: {} });
+    console.log(players.map(p => p.name));
+    expect(players).to.have.length(2);
 };
