6108fd82115f9c49fb74957bf38934d3a22c0e1c
hverlin <hugues.verlin@gmail.com>
2020-06-18 16:35:55 +0200

    Default scope with include is overridden when using reload

--- src/sscce.js
+++ src/sscce.js
@@ -11,19 +11,57 @@ const createSequelizeInstance = require('./utils/create-sequelize-instance');
 const log = require('./utils/log');
 
 // You can use chai assertions directly in your SSCCE if you want.
-const { expect } = require('chai');
+const { assert } = require('chai');
 
 // Your SSCCE goes inside this function.
-module.exports = async function() {
-    const sequelize = createSequelizeInstance({
-        logQueryParameters: true,
-        benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
-        }
-    });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
-    await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+module.exports = async function () {
+  const sequelize = createSequelizeInstance({
+    logQueryParameters: true,
+    benchmark: true,
+    define: { timestamps: false }
+  });
+
+  const Bar = sequelize.define('Bar', {
+    name: DataTypes.TEXT,
+    fooId: DataTypes.INTEGER
+  });
+
+  const Foo = sequelize.define('Foo', {
+    name: DataTypes.TEXT,
+  }, {
+    defaultScope: {
+      include: [{model: Bar}],
+    }
+  });
+
+  Bar.belongsTo(Foo, {
+    foreignKey: 'fooId',
+  });
+
+  Foo.hasMany(Bar, {
+    foreignKey: {
+      name: 'fooId',
+      allowNull: false,
+    },
+    onDelete: 'cascade',
+  });
+
+
+  await sequelize.sync();
+
+  const foo = await Foo.create({name: 'foo'});
+
+  const bar = await Bar.create({name:'bar', fooId: foo.id});
+
+  const fooFromFind = await Foo.findByPk(foo.id);
+  console.log(fooFromFind.toJSON())
+  assert.ok(fooFromFind.Bars)
+  assert.equal(fooFromFind.Bars[0].name, 'bar')
+
+  // here I would expect the defaultScope to be used as well
+  await foo.reload();
+
+  console.log(foo.toJSON())
+  assert.ok(foo.Bars)
+  assert.equal(foo.Bars[0].name, 'bar')
 };
