5ec82da0daedfbd1280d487b7e343390915818bf
Carson McManus <dyc3@users.noreply.github.com>
2020-06-23 17:52:42 -0400

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -122,7 +122,7 @@ module.exports = async function() {
     expect(e.name).to.equal("SequelizeUniqueConstraintError");
   }
   if (success) {
-    assert.fail("Successfully created user violating unique constraint on name");
+    assert.fail("Successfully created foo violating unique constraint on name");
   }
   
   success = false;
@@ -134,6 +134,6 @@ module.exports = async function() {
     expect(e.name).to.equal("SequelizeUniqueConstraintError");
   }
   if (success) {
-    assert.fail("Successfully created user violating unique constraint on email");
+    assert.fail("Successfully created foo violating unique constraint on email");
   }
 };
