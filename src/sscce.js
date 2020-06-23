8729614307846eb90321a56dcc981e07df0bad54
Carson McManus <dyc3@users.noreply.github.com>
2020-06-23 17:08:16 -0400

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -15,77 +15,105 @@ const { expect, assert } = require('chai');
 
 // Your SSCCE goes inside this function.
 module.exports = async function() {
-    const sequelize = createSequelizeInstance({
-        logQueryParameters: true,
-        benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
+  const sequelize = createSequelizeInstance({
+      logQueryParameters: true,
+      benchmark: true,
+      define: {
+          timestamps: false // For less clutter in the SSCCE
+      }
+  });
+  const Foo = sequelize.define('Foo', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    username: {
+      allowNull: false,
+      unique: true,
+      type: Sequelize.STRING,
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: Sequelize.STRING,
+    },
+    salt: {
+      allowNull: false,
+      type: Sequelize.BLOB,
+    },
+    hash: {
+      allowNull: false,
+      type: Sequelize.BLOB,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }, {
+    validate: {
+      ensureCredentials() {
+        if ((!this.email || !this.hash || !this.salt) && !this.discordId) {
+          throw new Error('Incomplete login credentials. Requires social login or email/password.');
         }
-    });
-    const Foo = sequelize.define('Foo', {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          len: [1, 255],
-        },
       },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        unique: true,
-        validate: {
-          isEmail: {
-            args: [
-              {
-                require_tld: false,
-              },
-            ],
-          },
-        },
-      },
-      salt: {
-        type: DataTypes.BLOB,
-        allowNull: true,
-        validate: {
-          len: [1, 256],
-        },
-      },
-      hash: {
-        type: DataTypes.BLOB,
-        allowNull: true,
-      },
-      discordId: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-    }, {
-      validate: {
-        ensureCredentials() {
-          if ((!this.email || !this.hash || !this.salt) && !this.discordId) {
-            throw new Error('Incomplete login credentials. Requires social login or email/password.');
-          }
-        },
-      },
-    });
+    },
+  });
+  await sequelize.sync();
   
-    await sequelize.sync();
-    log(await Foo.create({ email: "test@localhost", salt: Buffer.from([0]), hash: Buffer.from([0]), name: 'foo' }));
-    log(await Foo.create({ discordId: "12341234", name: 'foo2' }));
-    expect(await Foo.count()).to.equal(2);
-    try {
-      log(await Foo.create({ discordId: "12341234", name: 'foo' }));
-      assert.fail();
-    }
-    catch (e) {
-      expect(e.name).to.equal("SequelizeUniqueConstraintError");
-    }
-    try {
-      log(await Foo.create({ email: "test@localhost", salt: Buffer.from([0]), hash: Buffer.from([0]), name: 'foo3' }));
-      assert.fail();
-    }
-    catch (e) {
-      expect(e.name).to.equal("SequelizeUniqueConstraintError");
-    }
+  // Here, the unique constraint on email gets removed
+  let queryInterface = sequelize.getQueryInterface();
+  await queryInterface.addColumn('Users', 'discordId', {
+    type: Sequelize.STRING,
+    defaultValue: null,
+    allowNull: true,
+  });
+  await queryInterface.changeColumn('Users', 'email', {
+    type: Sequelize.STRING,
+    allowNull: true,
+  });
+  await queryInterface.changeColumn('Users', 'hash', {
+    type: Sequelize.BLOB,
+    allowNull: true,
+  });
+  await queryInterface.changeColumn('Users', 'salt', {
+    type: Sequelize.BLOB,
+    allowNull: true,
+  });
+  
+  // attempting to readd unique constraints
+  await queryInterface.changeColumn('Users', 'username', {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  });
+  await queryInterface.changeColumn('Users', 'email', {
+    type: Sequelize.STRING,
+    allowNull: true,
+    unique: true,
+  });
+
+  // test expected behavior
+  log(await Foo.create({ email: "test@localhost", salt: Buffer.from([0]), hash: Buffer.from([0]), name: 'foo' }));
+  log(await Foo.create({ discordId: "12341234", name: 'foo2' }));
+  expect(await Foo.count()).to.equal(2);
+  try {
+    log(await Foo.create({ discordId: "12341234", name: 'foo' }));
+    assert.fail();
+  }
+  catch (e) {
+    expect(e.name).to.equal("SequelizeUniqueConstraintError");
+  }
+  try {
+    log(await Foo.create({ email: "test@localhost", salt: Buffer.from([0]), hash: Buffer.from([0]), name: 'foo3' }));
+    assert.fail();
+  }
+  catch (e) {
+    expect(e.name).to.equal("SequelizeUniqueConstraintError");
+  }
 };
