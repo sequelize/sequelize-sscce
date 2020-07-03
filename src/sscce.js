75f7806daaf32e8ecd1ce98781d36519818098d0
dyuan-boxlabs <65084919+dyuan-boxlabs@users.noreply.github.com>
2020-07-02 21:10:25 -0400

    SSCCEE Upsert Returning Bug
    model.upsert(record, {returning: true}) does not return correct record for composite keys
    
    db: Postgres

--- src/sscce.js
+++ src/sscce.js
@@ -1,29 +1,46 @@
-'use strict';
+"use strict";
 
 // Require the necessary things from Sequelize
-const { Sequelize, Op, Model, DataTypes } = require('sequelize');
+const { Sequelize, Op, Model, DataTypes } = require("sequelize");
 
 // This function should be used instead of `new Sequelize()`.
 // It applies the config for your SSCCE to work on CI.
-const createSequelizeInstance = require('./utils/create-sequelize-instance');
+const createSequelizeInstance = require("./utils/create-sequelize-instance");
 
 // This is an utility logger that should be preferred over `console.log()`.
-const log = require('./utils/log');
+const log = require("./utils/log");
 
 // You can use chai assertions directly in your SSCCE if you want.
-const { expect } = require('chai');
+const { expect } = require("chai");
 
 // Your SSCCE goes inside this function.
-module.exports = async function() {
-    const sequelize = createSequelizeInstance({
-        logQueryParameters: true,
-        benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
-        }
-    });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
-    await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+module.exports = async function () {
+  if (process.env.DIALECT !== "postgres") return;
+
+  const sequelize = createSequelizeInstance({
+    logQueryParameters: true,
+    benchmark: true,
+    define: {
+      timestamps: false, // For less clutter in the SSCCE
+    },
+  });
+  const Foo = sequelize.define("Foo", {
+    key1: { type: DataTypes.BIGINT, allowNull: false, primaryKey: true },
+    key2: { type: DataTypes.BIGINT, allowNull: false, primaryKey: true },
+    data: DataTypes.TEXT,
+  });
+  await sequelize.sync();
+
+  const record1 = await Foo.upsert(
+    { key1: 1, key2: 1, data: "1" },
+    { returning: true }
+  );
+  const record2 = await Foo.upsert(
+    { key1: 1, key2: 2, data: "2" },
+    { returning: true }
+  );
+
+  log(record1[0].toString());
+  log(record2[0].toString());
+  expect(record1[0].toString()).to.not.equal(record2[0].toString());
 };
