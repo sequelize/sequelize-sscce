10680ad787e45a7ef2ab7879d5654ea659adc260
robyte-ctrl <66415808+robyte-ctrl@users.noreply.github.com>
2020-07-23 12:13:48 +0300

    issue test #1

--- src/sscce.js
+++ src/sscce.js
@@ -1,29 +1,35 @@
 'use strict';
 
-// Require the necessary things from Sequelize
 const { Sequelize, Op, Model, DataTypes } = require('sequelize');
 
-// This function should be used instead of `new Sequelize()`.
-// It applies the config for your SSCCE to work on CI.
 const createSequelizeInstance = require('./utils/create-sequelize-instance');
 
-// This is an utility logger that should be preferred over `console.log()`.
 const log = require('./utils/log');
 
-// You can use chai assertions directly in your SSCCE if you want.
-const { expect } = require('chai');
+// const { expect } = require('chai');
 
-// Your SSCCE goes inside this function.
+async function up(queryInterface, Sequelize) {
+  await queryInterface.changeColumn('tasks', 'status', { status: Sequelize.ENUM, values: ['new','in_progress','done'], allowNull: false, defaultValue: 'new' });
+}
+
+async function down(queryInterface, Sequelize) {
+  await queryInterface.changeColumn('tasks', 'status', { status: Sequelize.STRING, allowNull: false, defaultValue: '' });
+}
+
 module.exports = async function() {
     const sequelize = createSequelizeInstance({
         logQueryParameters: true,
         benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
-        }
+        define: { timestamps: false }
     });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+
+    const Task = sequelize.define('Task', { status: DataTypes.STRING, allowNull: false, defaultValue: '' }, { tableName: 'tasks' });
     await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+    
+    // The problem is with a migration
+    const queryInterface = sequelize.getQueryInterface();
+    
+    await up(queryInterface, Sequelize);
+  
+    await down(queryInteface, Sequelize);
 };
