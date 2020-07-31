d133e06a826133c784f4b00cdfe86580452b3f20
Nicolas S. Montanaro <nsmon@protonmail.ch>
2020-07-31 12:55:29 -0400

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -27,6 +27,6 @@ module.exports = async function() {
     const x = await Foo.create({ value: {type: "Point", coordinates: [45, 45] }});
     const y = await Foo.create({ value: {type: "Point", coordinates: [45, 45] }});
   
-    const z = Object.assign(x, y);
+    Object.assign(x, y);
     expect(x.changed().length.to.equal(0));
 };
