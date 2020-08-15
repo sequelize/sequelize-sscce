67fdec14a03c880b6ede81ffb2123073fee8d354
Alexandre Khoury <49289908+Mageek627@users.noreply.github.com>
2020-08-16 00:48:58 +0200

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -22,8 +22,9 @@ module.exports = async function() {
             timestamps: false // For less clutter in the SSCCE
         }
     });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+
+    sequelize.define('table1', {age: DataTypes.INTEGER }, { indexes: [{ fields: ['age'] }] });
+    sequelize.define('table2', {age: DataTypes.INTEGER }, { indexes: [{ fields: ['age'] }] });
+
     await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
 };
