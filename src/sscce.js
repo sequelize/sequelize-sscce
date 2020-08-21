f1c00fbc8c92004655b3b89a4b304c4362d6cfe3
Jean <marmainjean@gmail.com>
2020-08-21 14:00:57 +0200

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -10,6 +10,7 @@ const createSequelizeInstance = require('./utils/create-sequelize-instance');
 // This is an utility logger that should be preferred over `console.log()`.
 const log = require('./utils/log');
 
+
 // You can use chai assertions directly in your SSCCE if you want.
 const { expect } = require('chai');
 
