fe43d3ba4508382f6a2e3969ee7b1603085ec160
Jolifanto_Bambla <muskat.rabe+github@hotmail.com>
2020-09-01 15:10:04 +0200

    SSCCE for Model.update issue

--- src/sscce.js
+++ src/sscce.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Require the necessary things from Sequelize
-const { Sequelize, Op, Model, DataTypes } = require('sequelize');
+const {Sequelize, Op, Model, DataTypes} = require('sequelize');
 
 // This function should be used instead of `new Sequelize()`.
 // It applies the config for your SSCCE to work on CI.
@@ -11,19 +11,48 @@ const createSequelizeInstance = require('./utils/create-sequelize-instance');
 const log = require('./utils/log');
 
 // You can use chai assertions directly in your SSCCE if you want.
-const { expect } = require('chai');
+const {expect} = require('chai');
 
 // Your SSCCE goes inside this function.
-module.exports = async function() {
-    const sequelize = createSequelizeInstance({
-        logQueryParameters: true,
-        benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
+module.exports = async function () {
+  const sequelize = createSequelizeInstance({
+    logQueryParameters: true,
+    benchmark: true,
+    define: {
+      timestamps: false // For less clutter in the SSCCE
+    }
+  });
+
+  const errorMessage = 'username or birthay not set';
+  class User extends Model {}
+  User.init({
+    username: DataTypes.STRING,
+    birthday: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'user',
+    validate: {
+      usernameAndBirthdayMustBeSet() {
+        if (!this.username || !this.birthday) {
+          throw new Error(errorMessage);
         }
-    });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
-    await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+      }
+    }
+  });
+
+  await sequelize.sync();
+  const user = await User.create({
+    username: 'foo',
+    birthday: new Date(1980, 6, 20)
+  });
+
+  let err;
+  try {
+    await User.update({username: 'bar'}, {where: {username: user.username}});
+  } catch (_err) {
+    err = _err;
+  }
+  expect(err).to.exist;
+  expect(err).to.be.instanceOf(Sequelize.ValidationError);
+  expect(err.message).to.equal(errorMessage);
 };
