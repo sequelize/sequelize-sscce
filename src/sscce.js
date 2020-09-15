789dc2fb69af0a3d151f95b33aa3b00366cc5cfa
MCMrARM <mcmrarm@gmail.com>
2020-09-15 13:35:22 +0200

    [SSCCE] sqlite changeColumn two field primary keys

--- src/sscce.js
+++ src/sscce.js
@@ -1,19 +1,11 @@
 'use strict';
 
-// Require the necessary things from Sequelize
-const { Sequelize, Op, Model, DataTypes } = require('sequelize');
+if (process.env.DIALECT !== "postgres") return;
 
-// This function should be used instead of `new Sequelize()`.
-// It applies the config for your SSCCE to work on CI.
+const { Sequelize, DataTypes } = require('sequelize');
+
 const createSequelizeInstance = require('./utils/create-sequelize-instance');
 
-// This is an utility logger that should be preferred over `console.log()`.
-const log = require('./utils/log');
-
-// You can use chai assertions directly in your SSCCE if you want.
-const { expect } = require('chai');
-
-// Your SSCCE goes inside this function.
 module.exports = async function() {
     const sequelize = createSequelizeInstance({
         logQueryParameters: true,
@@ -22,8 +14,26 @@ module.exports = async function() {
             timestamps: false // For less clutter in the SSCCE
         }
     });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+    const Test = sequelize.define('test', {
+        a: {type: DataTypes.STRING, allowNull: false, primaryKey: true},
+        b: {type: DataTypes.STRING, allowNull: false, primaryKey: true},
+        c: {type: DataTypes.STRING, allowNull: false}
+    });
     await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+
+    await Test.create({
+        a: "test",
+        b: "one",
+        c: ""
+    });
+    await Test.create({
+        a: "test",
+        b: "two",
+        c: ""
+    });
+
+    await sequelize.getQueryInterface().changeColumn('tests', 'c', {
+        type: DataTypes.STRING,
+        allowNull: true
+    });
 };
