1fef3152e2008feff8e65494627fb5eb760116df
Supriyo Sarkar <supriyosarkar282@gmail.com>
2020-10-19 11:20:51 +0530

    sellsLead.js sscce
    error : UnhandledPromiseRejectionWarning: SequelizeValidationError: notNull Violation: Lead.id cannot be null

--- src/sscce.js
+++ src/sscce.js
@@ -1,29 +1,17 @@
 'use strict';
+module.exports = (Sequelize, DataTypes) => {
+  var Lead = Sequelize.define("Lead", {
+    id: {
+      allowNull: false, // This particular lines gives the error . 
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUID4,
+    },
 
-// Require the necessary things from Sequelize
-const { Sequelize, Op, Model, DataTypes } = require('sequelize');
-
-// This function should be used instead of `new Sequelize()`.
-// It applies the config for your SSCCE to work on CI.
-const createSequelizeInstance = require('./utils/create-sequelize-instance');
-
-// This is an utility logger that should be preferred over `console.log()`.
-const log = require('./utils/log');
-
-// You can use chai assertions directly in your SSCCE if you want.
-const { expect } = require('chai');
-
-// Your SSCCE goes inside this function.
-module.exports = async function() {
-    const sequelize = createSequelizeInstance({
-        logQueryParameters: true,
-        benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
-        }
-    });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
-    await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  });
+  return Lead;
 };
