5498047007fc3e32947f6037fd76aa9c0f877a5b
robyte-ctrl <66415808+robyte-ctrl@users.noreply.github.com>
2020-11-19 19:57:00 +0200

    SSCCE for issue #12850

--- src/sscce.js
+++ src/sscce.js
@@ -1,29 +1,37 @@
 'use strict';
 
-// Require the necessary things from Sequelize
 const { Sequelize, Op, Model, DataTypes } = require('sequelize');
 
-// This function should be used instead of `new Sequelize()`.
-// It applies the config for your SSCCE to work on CI.
 const createSequelizeInstance = require('./utils/create-sequelize-instance');
 
-// This is an utility logger that should be preferred over `console.log()`.
 const log = require('./utils/log');
 
-// You can use chai assertions directly in your SSCCE if you want.
-const { expect } = require('chai');
+const { strictEqual } = require('assert');
 
-// Your SSCCE goes inside this function.
+function test(Model) {
+  return () => Model.update({ id: undefined }, { where: { id: undefined }, returning: true });
+}
+
 module.exports = async function() {
     const sequelize = createSequelizeInstance({
         logQueryParameters: true,
         benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
-        }
     });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+  
+    const Foo = sequelize.define('Foo', { id: DataTypes.INTEGER, name: DataTypes.TEXT });
+  
     await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+  
+    let threwError = false, returnedEmptyArray = false;
+    try {
+      const id = undefined;
+      
+      const [,updated] = Foo.update({ id }, { where: { id }, returning: true });
+      
+      returnedEmptyArray = Array.isArray(updated) && updated.length === 0;
+    } catch (error) {
+       threwError = true;
+    }
+  
+    strictEqual(threwError || returnedEmptyArray, true);
 };
