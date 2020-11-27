3d5aaa816f6cd7def2680009662acaacffc94fec
matt-hra <37881822+matt-hra@users.noreply.github.com>
2020-11-27 12:47:04 -0800

    Conflict between attributes and quoteIdentifiers

--- src/sscce.js
+++ src/sscce.js
@@ -15,15 +15,56 @@ const { expect } = require('chai');
 
 // Your SSCCE goes inside this function.
 module.exports = async function() {
-    const sequelize = createSequelizeInstance({
-        logQueryParameters: true,
-        benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
-        }
-    });
-    const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
-    await sequelize.sync();
-    log(await Foo.create({ name: 'foo' }));
-    expect(await Foo.count()).to.equal(1);
+  const opts = {
+    define: {
+      timestamps: false // For less clutter in the SSCCE
+    }
+  }
+  const sequelize1 = createSequelizeInstance(opts)
+  const sequelize2 = createSequelizeInstance({...opts, quoteIdentifiers: false})
+
+  const Table1 = sequelize1.define('Table1', {
+    col: {
+      type: Sequelize.TEXT
+    }
+  })
+  const Table2 = sequelize2.define('Table2', {
+    col: {
+      type: Sequelize.TEXT
+    }
+  })
+
+  await Table1.sync()
+  await Table2.sync()
+
+  await Table1.create({
+    col: 'test value 1'
+  })
+  await Table2.create({
+    col: 'test value 2'
+  })
+
+  const [row1] = await Table1.findAll({
+    attributes: {
+      include: [[Sequelize.literal('1'), 'extraColumn']]
+    }
+  })
+  const [row2] = await Table2.findAll({
+    attributes: {
+      include: [[Sequelize.literal('2'), 'extraColumn']]
+    }
+  })
+  const [row3] = await Table2.findAll({
+    attributes: {
+      include: [[Sequelize.literal('3'), 'extra_column']]
+    }
+  })
+
+  log('row1', row1)
+  log('row2', row2)
+  log('row3', row3)
+  
+  expect(row1.dataValues).to.have.property('extraColumn') // works, includes the column extraColumn
+  expect(row2.dataValues).to.have.property('extraColumn') // fails, has no extra columns (but the query does)
+  expect(row3.dataValues).to.have.property('extra_column') // works, includes the column extra_column
 };
