4f6130d9dd75dd95f01a8c6dd95be2bdd105c167
arturoturris <49468723+arturoturris@users.noreply.github.com>
2020-12-21 14:07:20 -0600

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -15,7 +15,6 @@ const { expect } = require('chai');
 
 // Your SSCCE goes inside this function.
 module.exports = async function() {
-  if(process.env.DIALECT !== 'mysql') return;
     const sequelize = createSequelizeInstance({
         logQueryParameters: true,
         benchmark: true,
