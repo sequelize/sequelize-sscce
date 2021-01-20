fbbf99218f871108378939cd3eb7942015451f77
Maxime Charruel <m.charruel@gmail.com>
2021-01-20 08:43:47 +0100

    Update sscce.ts
    Duplicated foreign key with alter option when using PostgreSQL Schemas

--- src/sscce.ts
+++ src/sscce.ts
@@ -1,37 +1,53 @@
 // Require the necessary things from Sequelize
-import { Model, DataTypes } from 'sequelize';
+import { Model, DataTypes } from "sequelize";
 
 // This function should be used instead of `new Sequelize()`.
 // It applies the config for your SSCCE to work on CI.
-import createSequelizeInstance = require('./utils/create-sequelize-instance');
+import createSequelizeInstance = require("./utils/create-sequelize-instance");
 
 // This is an utility logger that should be preferred over `console.log()`.
-import log = require('./utils/log');
+import log = require("./utils/log");
 
 // You can use chai assertions directly in your SSCCE if you want.
-import { expect } from 'chai';
+import { expect } from "chai";
 
 // Your SSCCE goes inside this function.
 export async function run() {
-    const sequelize = createSequelizeInstance({
-        logQueryParameters: true,
-        benchmark: true,
-        define: {
-            timestamps: false // For less clutter in the SSCCE
-        }
-    });
+  if (process.env.DIALECT !== "postgres") return;
 
-    class Foo extends Model {};
-    Foo.init({
-        name: DataTypes.TEXT
-    }, {
-        sequelize,
-        modelName: 'Foo'
-    });
+  const sequelize = createSequelizeInstance({
+    logQueryParameters: true,
+    benchmark: true,
+    define: {
+      timestamps: false, // For less clutter in the SSCCE
+    },
+  });
 
-    await sequelize.sync();
+  class User extends Model {}
+  User.init(
+    {
+      name: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      schema: "schema1",
+      modelName: "User",
+    }
+  );
 
-    log(await Foo.create({ name: 'TS foo' }));
+  class Product extends Model {}
+  Product.init(
+    {
+      name: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      schema: "schema2",
+      modelName: "Product",
+    }
+  );
 
-    expect(await Foo.count()).to.equal(1);
+  Product.belongsTo(User);
+
+  await sequelize.sync({ alter: true });
 }
