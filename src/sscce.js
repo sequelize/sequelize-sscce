783c8cb7eafddebc4b754a3a7de9a4fd7ea30445
Riley James <github@rileyjames.co>
2021-07-31 16:35:12 +1200

    build & create with default boolean value set

--- src/sscce.js
+++ src/sscce.js
@@ -24,13 +24,17 @@ module.exports = async function() {
     }
   });
 
-  const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+  const Foo = sequelize.define('Foo', { name: DataTypes.TEXT, verified: { type: DataTypes.BOOLEAN, default: false } });
 
   const spy = sinon.spy();
   sequelize.afterBulkSync(() => spy());
   await sequelize.sync();
   expect(spy).to.have.been.called;
 
-  log(await Foo.create({ name: 'foo' }));
+  log(const fooInstance = await Foo.create({ name: 'foo' }));
   expect(await Foo.count()).to.equal(1);
+  expect(fooInstance.verified).to.equal(false);
+
+  log(const fooInstanceBuild = await Foo.build({ name: 'foo' }));
+  expect(fooInstanceBuild.verified).to.equal(false);
 };
