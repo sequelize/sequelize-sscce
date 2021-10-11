10f02a3048848f9dfdf0683eeeafa253993e404c
Domi <Domiii@users.noreply.github.com>
2021-10-12 05:27:55 +0800

    SSCE for issue 13554

--- src/sscce.js
+++ src/sscce.js
@@ -1,36 +1,52 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Require the necessary things from Sequelize
-const { Sequelize, Op, Model, DataTypes } = require('sequelize');
-
-// This function should be used instead of `new Sequelize()`.
-// It applies the config for your SSCCE to work on CI.
-const createSequelizeInstance = require('./utils/create-sequelize-instance');
-
-// This is an utility logger that should be preferred over `console.log()`.
-const log = require('./utils/log');
-
-// You can use sinon and chai assertions directly in your SSCCE if you want.
-const sinon = require('sinon');
+const path = require('path');
 const { expect } = require('chai');
+const { createSequelizeInstance } = require('./dev/sscce-helpers');
+const { Model, DataTypes } = require('.');
 
-// Your SSCCE goes inside this function.
-module.exports = async function() {
-  const sequelize = createSequelizeInstance({
-    logQueryParameters: true,
-    benchmark: true,
-    define: {
-      timestamps: false // For less clutter in the SSCCE
-    }
-  });
+class User extends Model { }
 
-  const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+async function main() {
+  try {
+    const sequelize = createSequelizeInstance({
+      benchmark: true,
+      retry: {
+        max: 0
+      }
+    });
+    User.init({
+      name: DataTypes.STRING,
+      age: DataTypes.INTEGER
+    }, { sequelize, modelName: 'user' });
+    await sequelize.sync({ force: true });
 
-  const spy = sinon.spy();
-  sequelize.afterBulkSync(() => spy());
-  await sequelize.sync();
-  expect(spy).to.have.been.called;
+    // this does not work (AV)
+    const [b1, b2] = await Promise.all([
+      User.findOrCreate({
+        where: { name: "b" },
+        defaults: { age: 1 }
+      }),
+      User.findOrCreate({
+        where: { name: "b" },
+        defaults: { age: 2 }
+      })
+    ]);
+    console.log(`Result: ${[b1?.[0]?.dataValues?.age, b2?.[0]?.dataValues?.age] || 'undefined'} === 1, 1`);
 
-  log(await Foo.create({ name: 'foo' }));
-  expect(await Foo.count()).to.equal(1);
-};
+
+    await sequelize.close();
+
+    // expect(jane.username).to.equal('janedoe');
+  }
+  catch (err) {
+    console.error('####### FAIL\n\n', err);
+    const result = await User.findAll({
+      where: { name: "b" }
+    });
+    console.log('##### RESULT:', result);
+  }
+}
+
+main();
