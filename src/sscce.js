4eb1bbdc37630706bdb4f4399f542c30e4cca1dc
JP <punshon.jm@outlook.com>
2022-01-04 20:42:10 +1000

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -21,16 +21,44 @@ module.exports = async function() {
     benchmark: true,
     define: {
       timestamps: false // For less clutter in the SSCCE
+      freezeTableName: true,
+      underscored: true,
     }
   });
 
-  const Foo = sequelize.define('Foo', { name: DataTypes.TEXT });
+  const Foos = sequelize.define('Foos', { name: DataTypes.TEXT });
+  
+  const Bars = sequelize.define('Bars', { name: DataTypes.TEXT, disabled: DataTypes.BOOLEAN, });
+  Bars.removeAttribute('id');
+  
+  Foos.hasMany(Bars, { foreignKey: 'fooId' });
+  Bars.belongsTo(Foos, { foreignKey: 'fooId' });
 
   const spy = sinon.spy();
   sequelize.afterBulkSync(() => spy());
   await sequelize.sync();
   expect(spy).to.have.been.called;
+  
+  const myFoo = await Foos.create({ name: 'foo' });
+  
+  const myBar = await Bars.create({
+    name: 'bar',
+    fooId: myFoo.id,
+    disabled: false,
+  });
+  
+  const myBarNow = await Bars.findOne({
+    where: {
+      fooId: myFoo.id,
+    },
+  });
+  
+  // This is where it is failing
+  await myBarNow.update({
+    disabled: true,
+  });
+  
+  const myBarNow2 = await Bars.findOne({ where: { id: myBar.id });
 
-  log(await Foo.create({ name: 'foo' }));
-  expect(await Foo.count()).to.equal(1);
+  expect(myBarNow2.disabled).to.equal(true);
 };
