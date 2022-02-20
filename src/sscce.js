9df08efb5a873ba6fefd0e2592d3b082c81dfdf4
dor6-deepcred <90688282+dor6-deepcred@users.noreply.github.com>
2022-02-20 13:03:07 +0200

    Update sscce.js

--- src/sscce.js
+++ src/sscce.js
@@ -1,7 +1,5 @@
 'use strict';
 
-if (process.env.DIALECT !== "postgres") return;
-
 // Require the necessary things from Sequelize
 const { Sequelize, Op, Model, DataTypes } = require('sequelize');
 
@@ -15,6 +13,8 @@ const log = require('./utils/log');
 // You can use sinon and chai assertions directly in your SSCCE if you want.
 const sinon = require('sinon');
 const { expect } = require('chai');
+
+if (process.env.DIALECT !== "postgres") return;
 
 // Your SSCCE goes inside this function.
 module.exports = async function() {
