0316176201ec65fbc6d2e38c0f0f2c63116e2a1f
Rochet2 <rochet2@post.com>
2022-09-09 00:20:07 +0300

    Update sscce-sequelize-6.ts

--- src/sscce-sequelize-6.ts
+++ src/sscce-sequelize-6.ts
@@ -1,7 +1,54 @@
-import { DataTypes, Model, QueryTypes } from 'sequelize';
+import { DataTypes, Model, QueryTypes, Hooks: { hooks }  } from 'sequelize';
 import { createSequelize6Instance } from '../setup/create-sequelize-instance';
 import { expect } from 'chai';
 import sinon from 'sinon';
+
+const hookTypes = {
+  beforeValidate: { params: 2 },
+  afterValidate: { params: 2 },
+  validationFailed: { params: 3 },
+  beforeCreate: { params: 2 },
+  afterCreate: { params: 2 },
+  beforeDestroy: { params: 2 },
+  afterDestroy: { params: 2 },
+  beforeRestore: { params: 2 },
+  afterRestore: { params: 2 },
+  beforeUpdate: { params: 2 },
+  afterUpdate: { params: 2 },
+  beforeSave: { params: 2, proxies: ['beforeUpdate', 'beforeCreate'] },
+  afterSave: { params: 2, proxies: ['afterUpdate', 'afterCreate'] },
+  beforeUpsert: { params: 2 },
+  afterUpsert: { params: 2 },
+  beforeBulkCreate: { params: 2 },
+  afterBulkCreate: { params: 2 },
+  beforeBulkDestroy: { params: 1 },
+  afterBulkDestroy: { params: 1 },
+  beforeBulkRestore: { params: 1 },
+  afterBulkRestore: { params: 1 },
+  beforeBulkUpdate: { params: 1 },
+  afterBulkUpdate: { params: 1 },
+  beforeFind: { params: 1 },
+  beforeFindAfterExpandIncludeAll: { params: 1 },
+  beforeFindAfterOptions: { params: 1 },
+  afterFind: { params: 2 },
+  beforeCount: { params: 1 },
+  beforeDefine: { params: 2, sync: true, noModel: true },
+  afterDefine: { params: 1, sync: true, noModel: true },
+  beforeInit: { params: 2, sync: true, noModel: true },
+  afterInit: { params: 1, sync: true, noModel: true },
+  beforeAssociate: { params: 2, sync: true },
+  afterAssociate: { params: 2, sync: true },
+  beforeConnect: { params: 1, noModel: true },
+  afterConnect: { params: 2, noModel: true },
+  beforeDisconnect: { params: 1, noModel: true },
+  afterDisconnect: { params: 1, noModel: true },
+  beforeSync: { params: 1 },
+  afterSync: { params: 1 },
+  beforeBulkSync: { params: 1 },
+  afterBulkSync: { params: 1 },
+  beforeQuery: { params: 2 },
+  afterQuery: { params: 2 }
+};
 
 // if your issue is dialect specific, remove the dialects you don't need to test on.
 export const testingOnDialects = new Set(['postgres', 'postgres-native']);
@@ -35,18 +82,38 @@ export async function run() {
     modelName: 'Foo',
   });
 
+  const spies = {};
+
+  Object.keys(hookTypes).forEach((hook) => {
+    const spy = sinon.spy();
+    spies[hook] = spy;
+    Foo.addHook(hook, () => {
+      console.log(hook)
+      spy()
+    });
+  })
+  
+  const resetSpies = () => {
+    Object.values(spies).forEach((spy) => {
+      spy.resetHistory();
+    });
+  }
+
   // You can use sinon and chai assertions directly in your SSCCE.
   const spy = sinon.spy();
   sequelize.afterBulkSync(() => spy());
   await sequelize.sync({ force: true });
   expect(spy).to.have.been.called;
   
+  resetSpies();
+  console.log("Upserting first")
+  
   const result1 = await sequelize.query(
     `
       INSERT INTO "Foos" (id, name)
       VALUES          (1, 'steve')
       ON CONFLICT (id) DO UPDATE
-      SET name='steve'
+      SET name='ben'
       RETURNING id, name
     `,
     {
@@ -54,19 +121,24 @@ export async function run() {
     }
   );
   
+  resetSpies();
+  console.log("Upserting second")
+  
   const result2 = await sequelize.query(
     `
-      -- HAX
       INSERT INTO "Foos" (id, name)
-      VALUES          (1, 'steve')
+      VALUES          (1, 'doug')
       ON CONFLICT (id) DO UPDATE
-      SET name='steve'
+      SET name='will'
       RETURNING id, name
     `,
     {
       type: QueryTypes.RAW,
     }
   );
+  
+  resetSpies();
+  console.log("End")
 
   console.log(result1, result2);
   expect(result1).to.deep.equal(result2);
