dcaa02d2b58adea9fa565ddbf550625a5176586f
Aleksandar Stanisic <aleksandar@tuta.io>
2023-02-02 19:33:34 +0100

    pg: `bulkCreate` returns string instead of `Date`

--- src/sscce-sequelize-7.ts
+++ src/sscce-sequelize-7.ts
@@ -1,81 +1,63 @@
 import { DataTypes, Model } from "@sequelize/core";
-import { createSequelize7Instance } from "../setup/create-sequelize-instance";
 import { expect } from "chai";
 import sinon from "sinon";
+import { createSequelize7Instance } from "../setup/create-sequelize-instance";
 
 // if your issue is dialect specific, remove the dialects you don't need to test on.
 export const testingOnDialects = new Set([
-  "mssql",
-  "sqlite",
-  "mysql",
-  "mariadb",
-  "postgres",
-  "postgres-native",
+	// "mssql",
+	// "sqlite",
+	// "mysql",
+	// "mariadb",
+	"postgres",
+	// "postgres-native",
 ]);
 
 // You can delete this file if you don't want your SSCCE to be tested against Sequelize 7
 
 // Your SSCCE goes inside this function.
 export async function run() {
-  // This function should be used instead of `new Sequelize()`.
-  // It applies the config for your SSCCE to work on CI.
-  const sequelize = createSequelize7Instance({
-    logQueryParameters: true,
-    benchmark: true,
-    define: {
-      // For less clutter in the SSCCE
-      timestamps: false,
-    },
-  });
+	// This function should be used instead of `new Sequelize()`.
+	// It applies the config for your SSCCE to work on CI.
+	const sequelize = createSequelize7Instance({
+		logQueryParameters: true,
+		benchmark: true,
 
-  class Post extends Model {}
-  class Author extends Model {}
+		define: {
+			// For less clutter in the SSCCE
+			timestamps: false,
+		},
+	});
 
-  Post.init(
-    {
-      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      content: DataTypes.TEXT,
-      authorId: DataTypes.INTEGER,
-      coAuthorId: DataTypes.INTEGER,
-    },
-    {
-      sequelize,
-      modelName: "Post",
-    }
-  );
+	class Post extends Model {
+		declare id?: number;
+		declare dateValue: Date;
+	}
 
-  Author.init(
-    {
-      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      name: DataTypes.TEXT,
-    },
-    {
-      sequelize,
-      modelName: "Author",
-    }
-  );
+	Post.init(
+		{
+			id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+			dateValue: DataTypes.DATE,
+		},
+		{
+			sequelize,
+			modelName: "Post",
+		},
+	);
 
-  Post.belongsTo(Author, {
-    foreignKey: "authorId",
-    as: "author",
-    targetKey: "id",
-    inverse: { as: "myBooks", type: "hasMany" },
-  });
+	// You can use sinon and chai assertions directly in your SSCCE.
+	const spy = sinon.spy();
+	sequelize.afterBulkSync(() => spy());
+	await sequelize.sync({ force: true });
+	expect(spy).to.have.been.called;
 
-  Post.belongsTo(Author, {
-    foreignKey: "coAuthorId",
-    as: "coAuthor",
-    targetKey: "id",
-    inverse: { as: "notMyBooks", type: "hasMany" },
-  });
+	const resCreate = await Post.create({ dateValue: new Date() });
 
-  // You can use sinon and chai assertions directly in your SSCCE.
-  const spy = sinon.spy();
-  sequelize.afterBulkSync(() => spy());
-  await sequelize.sync({ force: true });
-  expect(spy).to.have.been.called;
+	const resCreateBulk = await Post.bulkCreate([{ dateValue: new Date() }]);
+	const resCreateBulk0 = resCreateBulk[0]!;
 
-  // Not needed since sync not working
-  // console.log(await Post.create({ name: 'TS foo' }));
-  // expect(await Post.count()).to.equal(1);
+	expect(resCreate.dateValue).to.be.instanceOf(Date);
+	// `dateValue` should be `Date`, but it's `string`
+	expect(resCreateBulk0.dateValue).not.to.be.instanceOf(Date);
+	expect(resCreateBulk0.dateValue).to.be.instanceOf(Date);
 }
