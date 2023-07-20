2eff8f26b0eee7f7519140b9d04896e8250ebd9f
speexy <jennifer.meyer@posteo.de>
2023-07-20 12:19:41 +0200

    Update sscce-sequelize-7.ts
    check if passwords are returned

--- src/sscce-sequelize-7.ts
+++ src/sscce-sequelize-7.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 
 // if your issue is dialect specific, remove the dialects you don't need to test on.
-export const testingOnDialects = new Set(['mssql', 'sqlite', 'mysql', 'mariadb', 'postgres', 'postgres-native']);
+export const testingOnDialects = new Set([ 'postgres', 'postgres-native']);
 
 // You can delete this file if you don't want your SSCCE to be tested against Sequelize 7
 
@@ -23,11 +23,12 @@ export async function run() {
 
   class Foo extends Model {}
 
-  Foo.init({
-    name: DataTypes.TEXT,
+  User.init({
+    name: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Foo',
+    modelName: 'User',
   });
 
   // You can use sinon and chai assertions directly in your SSCCE.
@@ -36,6 +37,17 @@ export async function run() {
   await sequelize.sync({ force: true });
   expect(spy).to.have.been.called;
 
-  console.log(await Foo.create({ name: 'TS foo' }));
-  expect(await Foo.count()).to.equal(1);
+  const attributes = ['name'];
+  const created = await User.create({ name: 'Foo', password: 'pass' }, {returning: attributes});
+  expect(created.name).to.equal('Foo);
+  expect(created.password).to.be.undefined;
+  const updatedWithChange = await User.update({name:'Bar'}, {where: {id: created.id}, returning: attributes});
+  expect(updatedWithChange.name).to.equal('Bar');
+  expect(updatedWithChange.password).to.be.undefined;
+  const updatedNoChange = await User.update({name:undefined}, {where: {id: created.id}, returning: attributes});
+  expect(updatedWithChange.name).to.equal('Bar');
+  expect(updatedWithChange.password).to.be.undefined;
+  
+  //expect(await Foo.count()).to.equal(1);
+  await User.up
 }
