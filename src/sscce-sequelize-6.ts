71b7045a0aaf39d41d4cc8b988587995e3b98fcf
Arno DUBOIS <arno.du@orange.fr>
2023-08-12 15:53:33 +0000

    Add reproduction

--- src/sscce-sequelize-6.ts
+++ src/sscce-sequelize-6.ts
@@ -1,4 +1,4 @@
-import { DataTypes, Model } from 'sequelize';
+import { BelongsToGetAssociationMixin, DataTypes, ForeignKey, InferAttributes, InferCreationAttributes, Model } from 'sequelize';
 import { createSequelize6Instance } from '../setup/create-sequelize-instance';
 import { expect } from 'chai';
 import sinon from 'sinon';
@@ -21,14 +21,43 @@ export async function run() {
     },
   });
 
-  class Foo extends Model {}
+  class Foo extends Model<InferAttributes<Foo>, InferCreationAttributes<Foo>> {
+    declare id?: string;
+    declare name: string;
+    declare BarId: ForeignKey<Bar['id']>;
+    declare getBar: BelongsToGetAssociationMixin<Bar>;
+  }
+  class Bar extends Model<InferAttributes<Bar>, InferCreationAttributes<Bar>> {
+    declare id?: string;
+    declare name: string;
+  }
 
   Foo.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
     name: DataTypes.TEXT,
   }, {
     sequelize,
     modelName: 'Foo',
   });
+
+  Bar.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: DataTypes.TEXT,
+  }, {
+    sequelize,
+    modelName: 'Bar',
+  });
+
+  Bar.hasMany(Foo);
+  Foo.belongsTo(Bar);
 
   // You can use sinon and chai assertions directly in your SSCCE.
   const spy = sinon.spy();
@@ -36,6 +65,20 @@ export async function run() {
   await sequelize.sync({ force: true });
   expect(spy).to.have.been.called;
 
-  console.log(await Foo.create({ name: 'TS foo' }));
-  expect(await Foo.count()).to.equal(1);
+  const theBar = await Bar.create({ name: 'TS bar' });
+  const theFoo = await Foo.create({ name: 'TS foo', BarId: theBar.id });
+
+  // All is fine, BarId is correctly filled in the model, due to the create
+  expect(await theFoo?.getBar()).to.not.be.null;
+
+  // If have a function in my code that gives me the attributes and include automatically. But in some cases, I want to be
+  // manually able to lazy load a specific relationship
+  const theFreshFoo = await Foo.findOne({
+    where: {
+      name: theFoo.get("name")
+    },
+    attributes: ["name"]
+  })
+  // And when trying to load the relation, the relationship attribute is missing, so I simply get null
+  expect(await theFreshFoo?.getBar()).to.not.be.null;
 }
