aca889be93746a134c43cebef852b1539f6f7c1e
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2024-02-09 09:04:51 +0000

    Add files from core repo

--- setup/mysql/latest/start.sh
+++ setup/mysql/latest/start.sh
@@ -2,15 +2,14 @@
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
+docker compose -p sequelize-mysql-latest down --remove-orphans
+docker compose -p sequelize-mysql-latest up -d
 
-docker-compose -p sequelize-mysql-80 down --remove-orphans
-docker-compose -p sequelize-mysql-80 up -d
+./../../wait-until-healthy.sh sequelize-mysql-latest
 
-../../wait-until-healthy.sh sequelize-mysql-80
+sleep 5s
 
-docker exec sequelize-mysql-80 \
+docker exec sequelize-mysql-latest \
   mysql --host 127.0.0.1 --port 3306 -uroot -psequelize_test -e "GRANT ALL ON *.* TO 'sequelize_test'@'%' with grant option; FLUSH PRIVILEGES;"
 
-DIALECT=mysql node --loader ts-node/esm ../../check-database-running.ts
-
-echo "Local MySQL-8.0 instance is ready for Sequelize tests."
+DIALECT=mysql ts-node ../../check-connection.ts
