35b88955ed275532372585eda3656f253567a216
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2024-02-09 09:34:08 +0000

    Ignore all the engines

--- dev/prepare-ci.sh
+++ dev/prepare-ci.sh
@@ -3,15 +3,15 @@
 yarn install --ignore-engines;
 
 if [ "$DIALECT" = "postgres" ]; then
-  yarn add pg@^8 pg-hstore@^2 pg-types@^2;
+  yarn add pg@^8 pg-hstore@^2 pg-types@^2 --ignore-engines;
 elif [ "$DIALECT" = "postgres-native" ]; then
-  yarn add pg@^8 pg-hstore@^2 pg-types@^2 pg-native;
+  yarn add pg@^8 pg-hstore@^2 pg-types@^2 pg-native --ignore-engines;
 elif [ "$DIALECT" = "mysql" ]; then
-  yarn add mysql2@^2;
+  yarn add mysql2@^2 --ignore-engines;
 elif [ "$DIALECT" = "mariadb" ]; then
-  yarn add mariadb@^2;
+  yarn add mariadb@^2 --ignore-engines;
 elif [ "$DIALECT" = "sqlite" ]; then
-  yarn add sqlite3@^5;
+  yarn add sqlite3@^5 --ignore-engines;
 elif [ "$DIALECT" = "mssql" ]; then
-  yarn add tedious@^8;
+  yarn add tedious@^8 --ignore-engines;
 fi
