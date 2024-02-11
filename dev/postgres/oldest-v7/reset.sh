d9df307fea4125c6e6a39e12431312237c9f7c72
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2024-02-11 20:24:16 +0000

    Rename oldest to oldest-v7 and add oldest-v6

--- dev/postgres/oldest-v7/reset.sh
+++ dev/postgres/oldest-v7/reset.sh
@@ -2,6 +2,4 @@
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
-docker compose -p sequelize-postgres-oldest down --remove-orphans
-
-echo "Local oldest supported Postgres instance with postgis stopped (if it was running)."
+docker compose -p sequelize-postgres-oldest-v7 down --remove-orphans --volumes
